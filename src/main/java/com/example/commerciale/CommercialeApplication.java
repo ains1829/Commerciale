51c0d453ea4aa05f0ed95b88f6ded5f467769c84
ains1829 <andyrakotonavalona0@gmail.com>
2023-11-16 05:45:13 +0100

    first commit initiate project

---
+++ src/main/java/com/example/commerciale/CommercialeApplication.java
@@ -0,0 +1,13 @@
+package com.example.commerciale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercialeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommercialeApplication.class, args);
+	}
+
+}
