975b183fd3ae91ec4cd1281f7b664710d1f9afc1
ains1829 <andyrakotonavalona0@gmail.com>
2023-11-16 11:38:25 +0100

    commitapp

---
+++ src/main/java/com/example/commerciale/controller/MainController.java
@@ -0,0 +1,11 @@
+package com.example.commerciale.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class MainController {
+    @GetMapping("/maPage")
+    public String afficherMaPage() {
+        return "maPage";
+    }
+}
