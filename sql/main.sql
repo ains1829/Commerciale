f045f6b577d634ec1cdcf11090f3ad1828e4f11b
ains1829 <andyrakotonavalona0@gmail.com>
2023-11-16 08:51:22 +0100

    baseconfig

---
+++ sql/main.sql
@@ -0,0 +1,87 @@
+create database commerciale;
+CREATE TABLE Departement(
+   Id_Departement SERIAL,
+   nom VARCHAR(50) ,
+   PRIMARY KEY(Id_Departement)
+);
+
+CREATE TABLE membre(
+   Id_membre SERIAL,
+   nom VARCHAR(50)  NOT NULL,
+   email VARCHAR(50) ,
+   dtn DATE,
+   mdp VARCHAR(50) ,
+   Id_Departement INTEGER NOT NULL,
+   PRIMARY KEY(Id_membre),
+   FOREIGN KEY(Id_Departement) REFERENCES Departement(Id_Departement)
+);
+
+CREATE TABLE fournisseur(
+   Id_fournisseur SERIAL,
+   nom VARCHAR(50) ,
+   manager VARCHAR(50) ,
+   email VARCHAR(50) ,
+   adresse VARCHAR(50) ,
+   PRIMARY KEY(Id_fournisseur)
+);
+
+CREATE TABLE categorie(
+   Id_categorie SERIAL,
+   nomcategorie VARCHAR(50) ,
+   PRIMARY KEY(Id_categorie)
+);
+
+CREATE TABLE article(
+   Id_article SERIAL,
+   nom BIT VARYING(50) ,
+   unite VARCHAR(50) ,
+   Id_categorie INTEGER NOT NULL,
+   PRIMARY KEY(Id_article),
+   FOREIGN KEY(Id_categorie) REFERENCES categorie(Id_categorie)
+);
+
+CREATE TABLE stock(
+   Id_stock SERIAL,
+   quantite INTEGER,
+   datestock DATE,
+   Id_fournisseur INTEGER NOT NULL,
+   Id_article INTEGER NOT NULL,
+   PRIMARY KEY(Id_stock),
+   FOREIGN KEY(Id_fournisseur) REFERENCES fournisseur(Id_fournisseur),
+   FOREIGN KEY(Id_article) REFERENCES article(Id_article)
+);
+
+CREATE TABLE articleprix(
+   Id_articleprix SERIAL,
+   prixht DOUBLE PRECISION,
+   dateprix DATE,
+   tva DOUBLE PRECISION,
+   Id_fournisseur INTEGER NOT NULL,
+   Id_article INTEGER NOT NULL,
+   PRIMARY KEY(Id_articleprix),
+   FOREIGN KEY(Id_fournisseur) REFERENCES fournisseur(Id_fournisseur),
+   FOREIGN KEY(Id_article) REFERENCES article(Id_article)
+);
+
+CREATE TABLE proformat(
+   Id_proformat SERIAL,
+   dateproformat DATE,
+   quantite DOUBLE PRECISION,
+   prixunitaire DOUBLE PRECISION,
+   tva DOUBLE PRECISION,
+   Id_fournisseur INTEGER NOT NULL,
+   Id_article INTEGER NOT NULL,
+   PRIMARY KEY(Id_proformat),
+   FOREIGN KEY(Id_fournisseur) REFERENCES fournisseur(Id_fournisseur),
+   FOREIGN KEY(Id_article) REFERENCES article(Id_article)
+);
+CREATE TABLE besoin(
+   Id_besoin SERIAL,
+   datebesoin DATE,
+   quantite DOUBLE PRECISION,
+   Id_Departement INTEGER NOT NULL,
+   Id_article INTEGER NOT NULL,
+   PRIMARY KEY(Id_besoin),
+   FOREIGN KEY(Id_Departement) REFERENCES Departement(Id_Departement),
+   FOREIGN KEY(Id_article) REFERENCES article(Id_article)
+);
